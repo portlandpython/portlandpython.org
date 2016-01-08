3d9cf19e2f12859ef936bc9a6c0e5853940c01e8
Patrick Beeson <patrickbeeson@gmail.com>
2016-01-08 07:39:07 -0800

    Add secret key to environ variables for production.

--- portland_python/config/settings/production.py
+++ portland_python/config/settings/production.py
@@ -1,3 +1,4 @@
+from os import environ
 import json
 from unipath import Path
 import dj_database_url
@@ -7,19 +8,6 @@ from django.core.exceptions import ImproperlyConfigured
 from .base import *
 
 BASE_DIR = Path(__file__).ancestor(3)
-
-# secrets_path = BASE_DIR.child('config').child('settings', 'secrets.json')
-#
-# with open(secrets_path) as f:
-#     secrets = json.loads(f.read())
-#
-#
-# def get_secret(setting, secrets=secrets):
-#     try:
-#         return secrets[setting]
-#     except KeyError:
-#         error_msg = "Set the {} environment variable".format(setting)
-#         raise ImproperlyConfigured(error_msg)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -31,31 +19,6 @@ TIME_ZONE = 'America/Los_Angeles'
 
 DATABASES['default'] =  dj_database_url.config()
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': get_secret("DB_NAME"),
-#         'USER': 'gardenharmony',
-#         'PASSWORD': get_secret("DB_PASSWORD"),
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
-# SERVER_EMAIL = ''
-#
-# DEFAULT_FROM_EMAIL = ''
-#
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#
-# EMAIL_USE_TLS = True
-#
-# EMAIL_HOST = ''
-#
-# EMAIL_HOST_USER = ''
-#
-# EMAIL_HOST_PASSWORD = get_secret("EMAIL_HOST_PASSWORD")
-#
-# EMAIL_PORT = 587
+SECRET_KEY = environ.get('SECRET_KEY')
 
 COMPRESS_OFFLINE = True
